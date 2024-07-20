846495854acc38d8f109ec79c05a152f53405287
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-20 13:05:08 +0200

    add a title and lorem ipsum to test the container and the text-center class

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,9 @@
 <template>
-<body>
-    
+<body class="lmf-margin-top">
+    <div class="container">
+        <h1 class="text-center">La Mia Festa</h1>
+        <p class="mt-3 text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore aliquam, autem illum impedit cupiditate suscipit exercitationem minima assumenda, quibusdam optio pariatur nisi perspiciatis obcaecati. Omnis non praesentium illum, corrupti velit inventore ea voluptatem id eum dolorum numquam laborum qui nam autem dicta dolor aspernatur. Cumque totam quo, quasi dolor architecto maxime reprehenderit consectetur ipsa quod assumenda. A sequi sit consectetur perferendis recusandae eos et fugiat eaque! Possimus assumenda ea reiciendis neque non soluta at maiores iste. In cupiditate a explicabo suscipit. Quaerat, minus omnis accusantium explicabo itaque dolor incidunt, facere dignissimos debitis voluptatem maxime deleniti ea quam alias amet reprehenderit delectus in veniam. Fugit recusandae dolore optio minus cupiditate sint inventore? Iusto laudantium quisquam aliquam sunt reiciendis soluta, tempora, ratione esse consequuntur obcaecati eaque recusandae provident eius! Molestias ducimus, fugiat facere officiis eius alias pariatur perferendis eaque magni perspiciatis molestiae illum enim. Obcaecati, rem ut officiis voluptatem expedita tenetur dignissimos impedit placeat numquam laudantium odit repellendus vitae necessitatibus eos? Sit accusantium, officiis id error architecto eveniet excepturi ipsa ipsum quas reprehenderit reiciendis? Dicta id commodi non facilis, eos facere veritatis sed ipsa possimus error illum a. Sunt quaerat cum velit repudiandae voluptates perferendis fugit delectus? Magnam totam veniam aperiam saepe eveniet ipsam pariatur nam iste dicta, iure aliquam consectetur molestias non praesentium tempora eum sunt incidunt dolores exercitationem quidem? Aut asperiores, beatae nobis eligendi voluptatum reiciendis, voluptates minima doloremque quam non quos cupiditate perferendis. Cumque, aliquam. Voluptatum deserunt architecto minima similique accusamus eius aperiam ab aut ducimus sint eaque reiciendis dolorem repudiandae iusto, ad maiores suscipit ut facere at voluptate! Unde aut, quas doloremque ducimus maxime blanditiis dolore, velit itaque necessitatibus nam consectetur id sunt expedita ab at iure sequi perspiciatis deleniti ad aspernatur delectus magni! Eaque dolorem minima voluptas qui? Deserunt, sit tempore molestiae fuga voluptate sunt architecto perferendis et voluptas! Velit molestias expedita, eos quaerat voluptatum praesentium quis nisi minima dignissimos ratione temporibus, nobis, aliquam commodi magni deleniti unde. Pariatur iusto labore explicabo nostrum nam voluptate nobis nemo? Eum, nemo. Perspiciatis cum doloremque facilis quaerat non suscipit harum, eveniet voluptatem minima inventore debitis laborum ut at vero vel delectus tempora voluptatum adipisci rem saepe consectetur, nemo dolorum culpa. Laboriosam, cum, eos facere doloremque eligendi blanditiis dolorum, porro saepe consectetur unde recusandae ex perferendis. Aliquid id necessitatibus libero harum accusantium cumque, officiis quo veniam! Adipisci, officiis rem, quo laboriosam molestias ad enim perferendis quasi, molestiae id fugit harum ducimus dicta provident a suscipit nihil voluptates recusandae est quidem voluptatum impedit! Fugit nesciunt a magnam? Sed porro excepturi at, quod aperiam itaque repellendus ut quisquam commodi possimus recusandae officia cumque, vitae debitis, facilis nostrum perferendis quas ducimus sequi quaerat dignissimos aliquam ea quae voluptatibus? Non aperiam aliquid adipisci? Error velit hic quae commodi dolorem neque, sapiente libero nostrum quis, deleniti, voluptate consequatur delectus assumenda illo vero! Eveniet ratione vel, natus exercitationem officiis ducimus facere rem animi tempora maiores placeat, quis nemo magnam dicta dolorem. Eius sed quo optio dolorem quia neque praesentium laborum nisi in? Rem nemo nesciunt amet veniam?</p>
+    </div>
 </body>
 </template>
 
@@ -11,5 +14,13 @@
 </script>
 
 <style lang="scss" scoped>
+    @use '../assets/styles/partials/variables' as *;
 
+    body{
+        margin-top: -350px;
+    }
+
+    h1{
+        color: $gold;
+    }
 </style>
