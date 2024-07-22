8579a2742f964812152a9ce3e044407011333918
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-22 17:41:48 +0200

    imported OuttSpaceComponent in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <HeaderComponent />
   <MainComponent />
+  <OutSpaceComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import OutSpaceComponent from './components/OutSpaceComponent.vue';
   export default {
     name: 'App',
     components: {
       HeaderComponent,
-      MainComponent
+      MainComponent,
+      OutSpaceComponent
     }
     
   }
