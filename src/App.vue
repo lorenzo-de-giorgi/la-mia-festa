2c926c781c95549ebf9eff7ca6b3f6181ecda45c
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-23 12:35:17 +0200

    imported new component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <HeaderComponent />
   <MainComponent />
   <LeftImageComponent />
+  <RightImageComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 import LeftImageComponent from './components/LeftImageComponent.vue';
+import RightImageComponent from './components/RightImageComponent.vue';
   export default {
     name: 'App',
     components: {
       HeaderComponent,
       MainComponent,
-      LeftImageComponent
+      LeftImageComponent,
+      RightImageComponent
     }
     
   }
