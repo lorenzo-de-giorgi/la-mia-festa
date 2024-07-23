0cb32e5c253c96d5627856c5757d61553979c004
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-23 12:29:50 +0200

    updated the import for the renamed component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <HeaderComponent />
   <MainComponent />
-  <OutSpaceComponent />
+  <LeftImageComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
-import OutSpaceComponent from './components/OutSpaceComponent.vue';
+import LeftImageComponent from './components/LeftImageComponent.vue';
   export default {
     name: 'App',
     components: {
       HeaderComponent,
       MainComponent,
-      OutSpaceComponent
+      LeftImageComponent
     }
     
   }
