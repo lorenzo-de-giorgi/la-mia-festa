d65c5a6c9bd24481c08d6f93704bc1a5499d136a
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-26 20:53:14 +0200

    added Home to router.js

--- src/router.js
+++ src/router.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import App from './App.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        
+        {
+            path: '/',
+            name: 'home',
+            component: App,
+        },
     ]
 });
 export { router };
