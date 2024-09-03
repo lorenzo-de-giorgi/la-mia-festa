34c0ec55e6e344e257dbcd9bcc5ed50c23b0bd34
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-03 16:20:51 +0200

    added padding between h5 and form inputs

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <footer class="text-center mt-5">
-        <h6 class="fs-4 mt-2 pt-4">Vuoi organizzare una festa? Contattaci</h6>
+        <h6 class="fs-4 mt-2 pt-4 pb-2">Vuoi organizzare una festa? Contattaci</h6>
         <div class="container">
-            <form action="" class="form-group">
+            <form action="" class="form-group pb-3">
                 <input class="form-control mt-2" type="number" placeholder="Inserisci un numero di telefono per essere contattato">
                 <textarea class="form-control mt-2" type="text" placeholder="Inviaci un messaggio"></textarea>
                 <input type="submit" class="mt-2 btn btn-primary">
